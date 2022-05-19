867a6baef2e680f4f2ab0d4a6eadbbb592b22529
Qubaef <qubaefg@gmail.com>
2022-05-19 18:08:53 +0200

    Add option to specify spectrogram resolution

--- src/run_testing.py
+++ src/run_testing.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     metadata = processor.get_metadata()
 
     split_id = 2
-    model_id = "2"
+    model_id = "99"
 
     # Prepare data to disk for 3 different types of splits
-    model_path = "sample-training-2"
+    model_path = "dumb-spectro192-2"
     train, val, test = prepare_data(split_id, data_paths, processor, metadata)
     test_model(
         model_path,
